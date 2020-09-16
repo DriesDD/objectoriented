597ee1461049c63923c347757a46a78867782f76
DriesDD <driesdedecker@gmail.com>
2020-09-16 16:42:11 +0200

    echo alcoholpercentages

--- oop.php
+++ oop.php
@@ -5,42 +5,47 @@ class Beverage
     protected $color;
     protected $price;
     protected $temperature;
+    protected $name;
 
-    public function __construct(string $color, float $price, string $temperature = 'cold')
+    public function __construct(string $color, float $price, string $temperature = 'cold', string $name)
     {
         $this->color       = $color;
         $this->price       = $price;
         $this->temperature = $temperature;
+        $this->name       = $name;
     }
 
     public function getInfo(): string
     {
-        return 'This beverage is ' . $this->temperature . ' and ' . $this->color . '</br>';
+        return $this->name .' is ' . $this->temperature . ' and ' . $this->color . '</br>';
     }
 
 }
 
 class Beer extends Beverage
 {
-    private $name;
     private $alcoholpercentage;
 
-    public function __construct(string $color, float $price, string $temperature = 'cold', string $brewery, float $alcoholpercentage)
+    public function __construct(string $color, float $price, string $temperature = 'cold', string $name, float $alcoholpercentage)
     {
         $this->color       = $color;
         $this->price       = $price;
         $this->temperature = $temperature;
+        $this->name       = $name;
+        $this->alcoholpercentage = $alcoholpercentage;
     }
 
-    public function getInfo(): string
+    public function getAlcoholPercentage(): string
     {
-        return 'This beverage is ' . $this->temperature . ' and ' . $this->color . '</br>';
+        return 'The alcohol percentage of ' . $this->name . ' is ' . $this->alcoholpercentage . '%.</br>';
     }
 
 }
 
-$cola = new Beverage('black',2.50,'cold');
-$duvel = new Beer('blond,',3,'cold','moortgat',8.5);
+$cola = new Beverage('black',2.50,'cold','fritz cola');
+$duvel = new Beer('blond',3.50,'cold','duvel',8.5);
 
 echo ($cola -> getInfo());
 echo ($duvel -> getInfo());
+echo ($duvel -> getAlcoholPercentage());
+echo ($duvel->alcoholpercentage);
